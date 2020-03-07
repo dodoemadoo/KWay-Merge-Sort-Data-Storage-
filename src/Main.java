4df71dd16d183a8daa746c29ed605ae02f28a390
Fatima <44647655+fatima196@users.noreply.github.com>
2020-03-07 22:19:17 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,35 @@ import java.io.RandomAccessFile;
 
 public class Main {
     public static void main(String args[]) throws IOException {
+        Scanner input=new Scanner(System.in);
         KWayMergeSort k = new KWayMergeSort();
-        String[] runs = k.DivideInputFileIntoRuns("Index.bin",3);
-        k.SortEachRunOnMemoryAndWriteItBack(runs);
+        System.out.println("Enter size of runs");
+        try{ //to avoid invalid input. 
+        int numberOfRans= input.nextInt();
+        String[] runs = k.DivideInputFileIntoRuns("Index.bin",numberOfRans);
+        runs = k.SortEachRunOnMemoryAndWriteItBack(runs);
+        System.out.println("Enter number of runs you want to merge at time");
+        int KWayMerge= input.nextInt();
+        System.out.println("");
+        System.out.println("sorting...");
+        System.out.println("");
+        k.DoKWayMergeAndWriteASortedFile(runs,KWayMerge,"sorted.bin");
+        RandomAccessFile r = new RandomAccessFile("sorted.bin","r");
+        System.out.println("Sorted file: ");
+        for (int i = 0 ;i<64;i++)
+        {
+            System.out.println(i+" "+r.readInt()+" "+r.readInt());
+        }
+        System.out.println("Done!");
+        System.out.println("");
+        System.out.println("Enter key to search for");
+        int searchKey=input.nextInt();
+        int result=k.BinarySearchOnSortedFile("sorted.bin",searchKey);
+        if (result==-1)System.out.println("Sorry, Key not found :(' ");
+        else System.out.println("offset of Key "+searchKey+":  "+result);
+        r.close();
+        }catch(Exception e){
+            System.out.println("invalid input ");
+        }
     }
 }
